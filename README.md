e8332af972197d3698d7abbc951504edb0d6b921
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-02-14 22:26:53 +0300

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Recipe
+# Recipe Project
+
+### Tech Stack:
+- React
+- Redux
+- TailwindCSS
+- Axios
+- ReactFormHook
+- TanStack Query
+- DateFns
+
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-5xl font-primary text-primary ">hello</p>
+      <p className="text-5xl font-title text-secondary">hello</p>
+      <p className="text-5xl font-satisfy">hello</p>
+    </>
+  )
+}
+
+export default App
